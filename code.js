678fe65e4a990371e290d330335287df583b3630
Enoch Griffith <enoch2k2@hotmail.com>
2016-05-15 09:21:54 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(icecream) {
+  return "I love " + icecream;
+}
+
+function shouting() {
+  return "I LOVE JAVASCRIPT SOOOOOO MUCH";
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
